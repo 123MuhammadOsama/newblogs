a5d87685fcb22b60438c56ddd15695dd1d1cc612
MUHAMMAD OSAMA <developerscripters@gmail.com>
2024-12-19 17:58:17 +0500

    eslint

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unescaped-entities */
+
 import Image from "next/image";
 import { PortableText } from "@portabletext/react";
 import { client } from "@/sanity/lib/client";
